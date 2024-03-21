aa8c121283ea7f0a23f1e2a54a517dcc6cff6766
Youssef <ykmhabib97@gmail.com>
2024-03-21 06:20:40 +0200

    Replaced complex widget tree to fetch data from API with FutureBuilder

--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,50 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:news_app/models/article_model.dart';
 import 'package:news_app/services/news_service.dart';
 import 'package:news_app/widgets/news_list_view.dart';
 
-class NewsListViewBuilder extends StatefulWidget {
+class NewsListViewBuilder extends StatelessWidget {
   const NewsListViewBuilder({
     super.key,
   });
 
   @override
-  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
-}
-
-class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
-  bool isLoading = true;
-  List<ArticleModel> articles = [];
-
-  Future<void> getGeneralNews() async {
-    articles = await NewsService().getNews();
-    setState(() {
-      isLoading = false;
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    getGeneralNews();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? const SliverToBoxAdapter(
-            child: Center(
-              child: LinearProgressIndicator(),
-            ),
-          )
-        : articles.isNotEmpty
-            ? NewsListView(articles: articles)
-            : const SliverToBoxAdapter(
-                child: Center(
-                  child: Text(
-                    "Oops! There was an error.\nPlease try again later.",
-                  ),
-                ),
-              );
+    return FutureBuilder(
+      future: NewsService().getNews(),
+      builder: (context, snapshot) =>
+          NewsListView(articles: snapshot.data ?? []),
+    );
   }
 }
