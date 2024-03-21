cbaa239fa229d0545a244a6c791bd0c4845c5250
Youssef <ykmhabib97@gmail.com>
2024-03-21 17:34:34 +0200

    Created error message and refactored code to efficiently use the snapshot of the FutureBuilder

---
+++ lib/widgets/error_message.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ErrorMessage extends StatelessWidget {
+  final String message;
+
+  const ErrorMessage({
+    super.key,
+    required this.message,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
--- lib/widgets/news_list_view_builder.dart
+++ lib/widgets/news_list_view_builder.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/services/news_service.dart';
+import 'package:news_app/widgets/error_message.dart';
 import 'package:news_app/widgets/news_list_view.dart';
 
 class NewsListViewBuilder extends StatelessWidget {
@@ -11,8 +12,30 @@ class NewsListViewBuilder extends StatelessWidget {
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: NewsService().getNews(),
-      builder: (context, snapshot) =>
-          NewsListView(articles: snapshot.data ?? []),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SliverToBoxAdapter(
+            child: Center(
+              child: LinearProgressIndicator(),
+            ),
+          );
+        }
+
+        if (snapshot.hasError) {
+          ErrorMessage(
+            message: snapshot.error.toString(),
+          );
+        }
+
+        if (!snapshot.hasData) {
+          return const SliverToBoxAdapter(
+            child: Center(
+              child: Text("No Data to be displayed"),
+            ),
+          );
+        }
+        return NewsListView(articles: snapshot.data!);
+      },
     );
   }
 }
